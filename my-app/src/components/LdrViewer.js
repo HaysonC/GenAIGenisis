633e35aac7cf84d267f183ff27739091136f48b3
ShouArchie <archie.shou@gmail.com>
2025-03-23 04:00:16 -0400

    added al colours

--- my-app/src/components/LdrViewer.js
+++ my-app/src/components/LdrViewer.js
@@ -16,36 +16,153 @@ const AVAILABLE_BRICKS = [
 
 // LDR colors mapping (expanded and refined)
 const LDR_COLORS = {
-  0: 0x000000,   // Black
-  1: 0x0055BF,   // Blue
-  2: 0x00852B,   // Green
-  3: 0x009A4E,   // Dark Turquoise
-  4: 0xD01012,   // Red
-  5: 0xF5C518,   // Yellow
-  6: 0x02838F,   // Teal
-  7: 0xD67572,   // Pink
-  8: 0x8E5597,   // Purple
-  9: 0x57615B,   // Dark Gray
-  10: 0xF17626,  // Orange
-  11: 0x9DC9CA,  // Light Turquoise
-  14: 0xEEEEEE,  // White
-  15: 0x6B5A5A,  // Brown
-  16: 0xD3BB4C,  // Medium Nougat
-  17: 0x0E3E9A,  // Dark Blue
-  18: 0x069D9F,  // Dark Turquoise
-  19: 0x058547,  // Dark Green
-  20: 0x802E41,  // Dark Pink
-  21: 0xA5371F,  // Dark Red
-  22: 0x8C5C20,  // Dark Brown
-  23: 0x9BA19D,  // Medium Gray
-  24: 0xCDCDCD,  // Light Gray
-  25: 0xA06EB9,  // Lavender
-  26: 0xE4ADC8,  // Light Pink
-  27: 0xD67240,  // Medium Orange
-  28: 0xF3CF9B,  // Tan
-  29: 0xBDC6AD,  // Light Green
-  36: 0xE4CD9E,  // Bright Green
-  // More colors can be added as needed
+  0: 0x1B2A34,  // Black
+1: 0x1E5AA8,  // Blue
+2: 0x00852B,  // Green
+3: 0x069D9F,  // Dark_Turquoise
+4: 0xB40000,  // Red
+5: 0xD3359D,  // Dark_Pink
+6: 0x543324,  // Brown
+7: 0x8A928D,  // Light_Grey
+8: 0x545955,  // Dark_Grey
+9: 0x97CBD9,  // Light_Blue
+10: 0x58AB41,  // Bright_Green
+11: 0x00AAA4,  // Light_Turquoise
+12: 0xF06D61,  // Salmon
+13: 0xF6A9BB,  // Pink
+14: 0xFAC80A,  // Yellow
+15: 0xF4F4F4,  // White
+16: 0xFFFF80,  // Main_Colour
+17: 0xADD9A8,  // Light_Green
+18: 0xFFD67F,  // Light_Yellow
+19: 0xD7BA8C,  // Tan
+20: 0xAFBED6,  // Light_Violet
+22: 0x671F81,  // Purple
+23: 0x0E3E9A,  // Dark_Blue_Violet
+24: 0x7F7F7F,  // Edge_Colour
+25: 0xD67923,  // Orange
+26: 0x901F76,  // Magenta
+27: 0xA5CA18,  // Lime
+28: 0x897D62,  // Dark_Tan
+29: 0xFF9ECD,  // Bright_Pink
+30: 0xA06EB9,  // Medium_Lavender
+31: 0xCDA4DE,  // Lavender
+65: 0xFAC80A,  // Rubber_Yellow
+68: 0xFDC383,  // Very_Light_Orange
+69: 0x8A12A8,  // Bright_Reddish_Lilac
+70: 0x5F3109,  // Reddish_Brown
+71: 0x969696,  // Light_Bluish_Grey
+72: 0x646464,  // Dark_Bluish_Grey
+73: 0x7396C8,  // Medium_Blue
+74: 0x7FC475,  // Medium_Green
+75: 0x000000,  // Speckle_Black_Copper
+76: 0x635F61,  // Speckle_Dark_Bluish_Grey_Silver
+77: 0xFECCCF,  // Light_Pink
+78: 0xFFC995,  // Light_Nougat
+79: 0xEEEEEE,  // Milky_White
+83: 0x0A1327,  // Pearl_Black
+84: 0xAA7D55,  // Medium_Nougat
+85: 0x441A91,  // Medium_Lilac
+86: 0x7B5D41,  // Light_Brown
+89: 0x1C58A7,  // Blue_Violet
+92: 0xBB805A,  // Nougat
+100: 0xF9B7A5,  // Light_Salmon
+110: 0x26469A,  // Violet
+112: 0x4861AC,  // Medium_Violet
+115: 0xB7D425,  // Medium_Lime
+118: 0x9CD6CC,  // Aqua
+120: 0xDEEA92,  // Light_Lime
+125: 0xF9A777,  // Light_Orange
+128: 0xAD6140,  // Dark_Nougat
+132: 0x000000,  // Speckle_Black_Silver
+133: 0x000000,  // Speckle_Black_Gold
+134: 0x764D3B,  // Copper
+135: 0xA0A0A0,  // Pearl_Light_Grey
+142: 0xDEAC66,  // Pearl_Light_Gold
+147: 0x83724F,  // Pearl_Dark_Gold
+148: 0x484D48,  // Pearl_Dark_Grey
+150: 0x989B99,  // Pearl_Very_Light_Grey
+151: 0xC8C8C8,  // Very_Light_Bluish_Grey
+176: 0x945148,  // Pearl_Red
+178: 0xAB673A,  // Pearl_Yellow
+179: 0x898788,  // Pearl_Silver
+183: 0xF6F2DF,  // Pearl_White
+187: 0x57392C,  // Pearl_Brown
+189: 0xAC8247,  // Reddish_Gold
+191: 0xFCAC00,  // Bright_Light_Orange
+212: 0x9DC3F7,  // Bright_Light_Blue
+216: 0x872B17,  // Rust
+218: 0x8E5597,  // Reddish_Lilac
+219: 0x564E9D,  // Lilac
+226: 0xFFEC6C,  // Bright_Light_Yellow
+232: 0x77C9D8,  // Sky_Blue
+256: 0x1B2A34,  // Rubber_Black
+272: 0x19325A,  // Dark_Blue
+273: 0x1E5AA8,  // Rubber_Blue
+288: 0x00451A,  // Dark_Green
+295: 0xFF94C2,  // Flamingo_Pink
+297: 0xAA7F2E,  // Pearl_Gold
+308: 0x352100,  // Dark_Brown
+313: 0xABD9FF,  // Maersk_Blue
+320: 0x720012,  // Dark_Red
+321: 0x469BC3,  // Dark_Azure
+322: 0x68C3E2,  // Medium_Azure
+323: 0xD3F2EA,  // Light_Aqua
+324: 0xB40000,  // Rubber_Red
+326: 0xE2F99A,  // Yellowish_Green
+330: 0x77774E,  // Olive_Green
+335: 0x88605E,  // Sand_Red
+350: 0xD67923,  // Rubber_Orange
+351: 0xF785B1,  // Medium_Dark_Pink
+353: 0xFF6D77,  // Coral
+366: 0xD86D2C,  // Earth_Orange
+368: 0xEDFF21,  // Neon_Yellow
+370: 0x755945,  // Medium_Brown
+371: 0xCCA373,  // Medium_Tan
+373: 0x75657D,  // Sand_Purple
+375: 0x8A928D,  // Rubber_Light_Grey
+378: 0x708E7C,  // Sand_Green
+379: 0x70819A,  // Sand_Blue
+402: 0xCA4C0B,  // Reddish_Orange
+406: 0x19325A,  // Rubber_Dark_Blue
+422: 0x915C3C,  // Sienna_Brown
+423: 0x543F33,  // Umber_Brown
+449: 0x671F81,  // Rubber_Purple
+450: 0xD27744,  // Fabuland_Brown
+462: 0xF58624,  // Medium_Orange
+484: 0x91501C,  // Dark_Orange
+490: 0xA5CA18,  // Rubber_Lime
+493: 0x656761,  // Magnet
+494: 0xD0D0D0,  // Electric_Contact_Alloy
+495: 0xAE7A59,  // Electric_Contact_Copper
+496: 0x969696,  // Rubber_Light_Bluish_Grey
+503: 0xBCB4A5,  // Very_Light_Grey
+504: 0x898788,  // Rubber_Flat_Silver
+507: 0xFA9C1C,  // Light_Orange_Brown
+508: 0xC65127,  // Fabuland_Red
+509: 0xCF8A47,  // Fabuland_Orange
+510: 0x78FC78,  // Fabuland_Lime
+511: 0xF4F4F4,  // Rubber_White
+10000: 0xF8F3E4,  // Fabric_Cream
+10002: 0x00852B,  // Rubber_Green
+10010: 0x58AB41,  // Rubber_Bright_Green
+10019: 0xD7BA8C,  // Rubber_Tan
+10026: 0x901F76,  // Rubber_Magenta
+10029: 0xFF9ECD,  // Rubber_Bright_Pink
+10030: 0xA06EB9,  // Rubber_Medium_Lavender
+10031: 0xCDA4DE,  // Rubber_Lavender
+10070: 0x5F3109,  // Rubber_Reddish_Brown
+10072: 0x646464,  // Rubber_Dark_Bluish_Grey
+10073: 0x7396C8,  // Rubber_Medium_Blue
+10078: 0xFFC995,  // Rubber_Light_Nougat
+10226: 0xFFEC6C,  // Rubber_Bright_Light_Yellow
+10308: 0x352100,  // Rubber_Dark_Brown
+10320: 0x720012,  // Rubber_Dark_Red
+10321: 0x469BC3,  // Rubber_Dark_Azure
+10322: 0x68C3E2,  // Rubber_Medium_Azure
+10323: 0xD3F2EA,  // Rubber_Light_Aqua
+10378: 0x708E7C,  // Rubber_Sand_Green
+10484: 0x91501C,  // Rubber_Dark_Orange
 };
 
 /* 
